9110685bfda5e8f59e49e9f05de9e5e20419d3ce
Alex Eng <aeng@ooyala.com>
2016-05-05 14:13:58 -0700

    [PBI-1658] -mocked adobe functions

---
+++ test/unit-test-helpers/mock_adobe.js
@@ -0,0 +1,97 @@
+ADB = {
+  va: {
+    plugins: {
+      videoplayer: {
+        VideoPlayerPlugin: function()
+        {
+          this.configure = function(){};
+          this.trackVideoLoad = function()
+          {
+          };
+
+          this.trackVideoUnload = function()
+          {
+          };
+          this.trackSessionStart = function()
+          {
+          };
+          this.trackPlay = function()
+          {
+          };
+          this.trackPause = function()
+          {
+          };
+          this.trackComplete = function()
+          {
+          };
+          this.trackSeekStart = function()
+          {
+          };
+          this.trackSeekComplete = function()
+          {
+          };
+          this.trackBufferStart = function()
+          {
+          };
+          this.trackBufferComplete = function()
+          {
+          };
+
+          this.trackAdStart = function()
+          {
+          };
+          this.trackAdComplete = function()
+          {
+          };
+
+          this.trackChapterStart = function(){};
+          this.trackChapterComplete = function(){};
+
+          this.trackBitrateChange = function(){};
+
+          this.trackVideoPlayerError = function(){};
+          this.trackApplicationError = function(){};
+        },
+        VideoPlayerPluginConfig: function()
+        {
+
+        },
+        AssetType: {
+          
+        }
+      },
+      aa: {
+        AdobeAnalyticsPlugin: function()
+        {
+          this.configure = function(){};
+        },
+        AdobeAnalyticsPluginConfig: function(){},
+        AdobeAnalyticsPluginDelegate: function(){}
+      },
+      ah: {
+        AdobeHeartbeatPlugin: function()
+        {
+          this.configure = function(){};
+        },
+        AdobeHeartbeatPluginConfig: function(){},
+        AdobeHeartbeatPluginDelegate: function(){}
+      }
+    },
+    Heartbeat: function()
+    {
+      this.configure = function(){};
+    },
+    HeartbeatConfig: function(){},
+    HeartbeatDelegate: function(){}
+  }
+};
+
+Visitor = function()
+{
+
+};
+
+AppMeasurement = function()
+{
+
+};
