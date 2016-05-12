9aa278f98ba698bff73329e3b98a27005780d879
dchau <dchau@ooyala.com>
2016-05-12 11:02:16 -0700

    [PBI-1725] Allow empty string as a string value in checkDataType

--- js/framework/AnalyticsConstants.js
+++ js/framework/AnalyticsConstants.js
@@ -544,7 +544,7 @@ if (!OO.Analytics.EVENT_DATA)
       var expectedType = expectedTypes[i];
       if (expectedType === "string")
       {
-        if (toRet && OO._.isString(toRet))
+        if (OO._.isString(toRet))
         {
           error = false;
           break;
