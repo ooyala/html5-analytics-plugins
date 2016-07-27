88e1e50e965629bab62bf092e1a36f0c538327b1
Alex Eng <aeng@ooyala.com>
2016-07-26 17:38:09 -0700

    [PBI-1871] -added a failsafe to destroy the plugin when the page gets closed by the user. This cleans up the existing Conviva session

--- js/plugins/conviva.js
+++ js/plugins/conviva.js
@@ -93,6 +93,12 @@ var ConvivaAnalyticsPlugin = function(framework)
         this.processEvent(recordedEvent.eventName, recordedEvent.params);
       }, this));
     }
+
+    //Conviva wants to know when sessions end when the page is closed
+    //Adding this beforeunload event listener as a failsafe
+    window.addEventListener('beforeunload', _.bind(function(){
+      this.destroy();
+    }, this));
   };
 
   /**
