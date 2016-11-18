4c6099a88d34763e7b662084e772db4dd6f67381
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2016-11-18 02:36:38 +0000

    update analytics plugins player version to 4.10.3

--- js/plugins/conviva.js
+++ js/plugins/conviva.js
@@ -14,7 +14,7 @@ var ConvivaAnalyticsPlugin = function(framework)
   var id;
 
   var OOYALA_PLAYER_VENDOR = "Ooyala";
-  var OOYALA_PLAYER_VERSION = "4.10.2";
+  var OOYALA_PLAYER_VERSION = "4.10.3";
 
   var currentConvivaSessionKey = null;
   var streamUrl = null;
