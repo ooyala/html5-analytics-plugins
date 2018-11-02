49e3c62cc53143cbb4c0cc5f2168a68c7e947730
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2018-11-02 17:53:56 +0000

    update analytics plugins player version to 4.29.2

--- js/plugins/omniture.js
+++ js/plugins/omniture.js
@@ -16,7 +16,7 @@ var OmnitureAnalyticsPlugin = function (framework)
   var id;
 
   var OOYALA_PLAYER_NAME = "Ooyala V4";
-  var OOYALA_PLAYER_VERSION = "";
+  var OOYALA_PLAYER_VERSION = "4.29.2";
 
   var playerDelegate = new OoyalaPlayerDelegate();
   var appMeasurement = null;
